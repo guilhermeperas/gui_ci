0a2810226491ae32c9d3203c76d45cec093cd68e
Guilherme Pereira <pguilherme926@gmail.com>
2023-04-26 16:22:36 +0100

    biblioteca de imagens casa arder

--- Code1PROF/application/controllers/BaseUpload.php
+++ Code1PROF/application/controllers/BaseUpload.php
@@ -34,7 +34,7 @@ class BaseUpload extends CI_Controller
             // basicamente com o objeto upload faziamos 
             if($this->input->post('thumbnail')){ 
                 // $mythumbnail = $Upload_Thumbnain->setupThumbnail($this->input->post('thumbnail'))
-                $configThumbnail['souce_image'] = $data['info_upload']['full_path']; // mudar isto para o seu proprio objeto
+                $configThumbnail['source_image'] = $data['info_upload']['full_path']; // mudar isto para o seu proprio objeto
                 // define a largura da thumbnail
                 $configThumbnail['maintain_ratio'] = TRUE;  
                 // se o $configThumbnail['maintain_ratio'] for definido como TRUE, ELE ESCOLHE O MENOR VALOR ENTRE ALTURA E LARGURA
@@ -53,9 +53,62 @@ class BaseUpload extends CI_Controller
                         $data['info_upload']['full_path']."/thumbs/".$data['info_upload']['raw_name']."_thumb".$data['info_upload']['file_ext'];
                 }
             }
+            if($this->input->post('width') || $this->input->post('height') ){
+                $configResize['source_image'] = $data['info_upload']['full_path'];
+                $configResize['maintain_ratio'] = ($this->input->post('ratio')) ? TRUE : FALSE;
+                $configResize['width'] =  ($this->input->post('width')) ? $this->input->post('width') : null;
+                $configResize['height'] =  ($this->input->post('height')) ? $this->input->post('height') : null;
+
+                $resize = $this->ResizeImage($configResize);
+                if(!$resize['status']){
+                    $data['info'] = "<br/> NÃO FOI POSSIVEL GERAR O resize DEVIDO AOS ERROS ABAIXO:<br/>";
+                    $data['info'] .= $resize['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = 
+                        $data['info_upload']['full_path']."/resized/".$data['info_upload']['raw_name']."_resize".$data['info_upload']['file_ext'];
+                }
+            }
+            if($this->input->post('rotation')){
+                $configRotation['source_image'] = $data['info_upload']['full_path'];
+                $configRotation['rotation_angle'] = $this->input->post('rotation');
+
+                $rotate = $this->RotateImage($configRotation);
+                if(!$rotate['status']){
+                    $data['info'] = "<br/> NÃO FOI POSSIVEL GERAR O rotate DEVIDO AOS ERROS ABAIXO:<br/>";
+                    $data['info'] .= $rotate['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = 
+                        $data['info_upload']['full_path']."/rotated/".$data['info_upload']['raw_name']."_rotated".$data['info_upload']['file_ext'];
+                }
+            }
+            if($this->input->post('crop')){
+                $configCrop['source_image'] = $data['info_upload']['full_path'];
+
+                $crop = $this->CropImage($configCrop,$data);
+                if(!$crop['status']){
+                    $data['info'] = "<br/> NÃO FOI POSSIVEL GERAR O crop DEVIDO AOS ERROS ABAIXO:<br/>";
+                    $data['info'] .= $crop['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = 
+                        $data['info_upload']['full_path']."/croped/".$data['info_upload']['raw_name']."_crop".$data['info_upload']['file_ext'];
+                }
+            }
+            if($this->input->post('watermark')){
+                $confgWM['source_image'] = $data['info_upload']['full_path'];
+
+                $WM = $this->ApplyWatermark($confgWM);
+                if(!$WM['status']){
+                    $data['info'] = "<br/> NÃO FOI POSSIVEL GERAR O crop DEVIDO AOS ERROS ABAIXO:<br/>";
+                    $data['info'] .= $WM['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = 
+                        $data['info_upload']['full_path']."/WM/".$data['info_upload']['raw_name']."_wm".$data['info_upload']['file_ext'];
+                }
+            }
         }
         $this->load->view("upload", $data);
     }
+    // estas funções podem ser so uma
     private function generateThumbnail($config){
         $config['image_library'] = 'gd2';
         $config['create_thumb'] = TRUE;
@@ -70,7 +123,81 @@ class BaseUpload extends CI_Controller
             $data['status'] = false;
             $data['message'] = $this->image_lib->display_errors();
         }
+        $this->image_lib->clear();
         return $data;
-
     }
+    private function ResizeImage($config){
+        $config['image_library'] = 'gd2';
+        $config['create_thumb'] = FALSE;
+        $config['new_image'] = "./uploads/resized/";
+
+        $this->image_lib->initialize($config);
+
+        if($this->image_lib->resize()){
+            $data['status'] = true;
+            $data['message'] = null;
+        }else{
+            $data['status'] = false;
+            $data['message'] = $this->image_lib->display_errors();
+        }
+        $this->image_lib->clear();
+        return $data;
+    }
+    private function RotateImage($config){
+        $config['image_library'] = 'gd2';
+        $config['create_thumb'] = FALSE;
+        $config['new_image'] = "./uploads/rotated/"; // tem de ser dinamico
+
+        $this->image_lib->initialize($config);
+        if($this->image_lib->rotate()){
+            $data['status'] = true;
+            $data['message'] = null;
+        }else{
+            $data['status'] = false;
+            $data['message'] = $this->image_lib->display_errors();
+        }
+        $this->image_lib->clear();
+        return $data;
+    }
+    private function CropImage($config,$data){
+        $config['image_library'] = 'gd2';
+        $config['new_image'] = "./uploads/croped/"; // tem de ser dinamico
+        $config['width'] = $data['info_upload']['image_width'] / 2; // dividimos a imagem a metade 
+        $config['height'] = $data['info_upload']['image_height'] / 2;
+
+        $config['x_axis'] = round($config['width'] / 2);
+        $config['y_axis'] = round($config['height'] / 2);
+
+        $this->image_lib->initialize($config);
+        if($this->image_lib->crop()){
+            $data['status'] = true;
+            $data['message'] = null;
+        }else{
+            $data['status'] = false;
+            $data['message'] = $this->image_lib->display_errors();
+        }
+        $this->image_lib->clear();
+        return $data;
+    }
+    private function ApplyWatermark($config){
+        $config['image_library'] = 'gd2';
+        $config['new_image'] = "./uploads/WM/"; // tem de ser dinamico
+
+        $config['wm_type'] = 'overlay';
+        $config['wm_opacity'] = '50';
+        $config['wm_overlay_path'] = './assets/images/watermark.png'; // 
+
+
+        $this->image_lib->initialize($config);
+        if($this->image_lib->watermark()){
+            $data['status'] = true;
+            $data['message'] = null;
+        }else{
+            $data['status'] = false;
+            $data['message'] = $this->image_lib->display_errors();
+        }
+        $this->image_lib->clear();
+        return $data;
+    }
+        // -----------------------------------------
 }
--- Code1PROF/application/views/upload.php
+++ Code1PROF/application/views/upload.php
@@ -4,8 +4,6 @@
             <?
                 if(isset($info))
                     echo $info;
-                if(isset($info_upload))
-                    print_r($info_upload);
             ?>
 			<div class="inner">
 				<h1 class="menu">Menu</h1>
@@ -21,14 +19,49 @@
                 <form action="<?=base_url('baseupload/upload')?>" method="POST" enctype="multipart/form-data">
                     <label>
                         <input type="file" name="image"/>
-                        <input type="submit" value="Processar"/>
                     </label>
                     <div class="checkbox">
                         <label>
                             <input type="checkbox" name="thumbnail">Criar thumbnail 
                         </label>
+                    </div>
+                    <label>Altura da imagem após redimensionar (em pixels)</label>
+                    <input type="number" name="height" class="form-control" />
+                    <label>largura da imagem após redimensionar (em pixels)</label>
+                    <input type="number" name="width" class="form-control" />
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="ratio">Manter proporção 
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>Rodar Imagem?
+                            <select name="rotation" class="form-control">
+                                <option value="">Nao Rodar</option>
+                                <option value="90">90</option>
+                                <option value="180">180</option>
+                                <option value="270">270</option>
+                                <option value="hor">Horizontal</option>
+                                <option value="vrt">Vertical</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div class="form-control">
+
+                        <label>
+                            <input type="checkbox" name="crop">Recortar Imagem? 
+                        </label>
+                        
+                    </div>
+                    <div class="form-control">
+                        
+                        <label>
+                            <input type="checkbox" name="watermark">Marca de Agua? 
+                        </label>
+                        
+                    </div>
+                    <input type="submit" value="Processar"/>
                 </form>
             </div>
 		</div>	
-        <? phpinfo()?>
 <?php $this->load->view('comuns/footer'); ?>
