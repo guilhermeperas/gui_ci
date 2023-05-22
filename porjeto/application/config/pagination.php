09b2341abf8618abfdaff47848b28daaee07257a
guilherme pereira <pguilherme824@gmail.com>
2023-05-22 21:20:07 +0100

    pagination cfg

--- porjeto/application/config/pagination.php
+++ porjeto/application/config/pagination.php
@@ -1,7 +1,4 @@
 <?
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// $config["base_url"] = base_url()."clientes";
-// $config["per_page"] = 2;
-// $config["uri_segment"] = 2; // na posição 0(page 0), uma vez que, não temos referencia para pagina dará erro no caso do null para valor inteiro.
 ?>
