2bb1deee7df50608a071d5b925bd1a9d7028e893
guilherme pereira <pguilherme824@gmail.com>
2023-05-25 17:16:41 +0100

    MEDICOS / ENFERMEIROS BACK OFFICE NEXT

--- porjeto/application/libraries/FileLoader.php
+++ porjeto/application/libraries/FileLoader.php
@@ -29,40 +29,25 @@ class FileLoader { //  mudar isto para o MY_CONTROLLER
     }
     public function loadBackOfficeView($view,$values,$tipo){ // nao deveria existir.......
 
-        switch($tipo){
-            case 'admin': 
+        $this->data['menuRoutes'] = array();
+        if($tipo === 'admin'){
                 $this->data['menuRoutes'] = array(
                     array('name' => 'Users','path' => base_url('backoffice/users')),
-                    array('name' => 'Medicos','path' => base_url('backoffice/medicos')),
-                    array('name' => 'Enfermeiros','path' => base_url('backoffice/enfermeiros')),
-                    array('name' => 'Utentes','path' => base_url('backoffice/utentes')),
-                    array('name' => 'Receitas','path' => base_url('backoffice/receitas')),
                     array('name' => 'Produtos','path' => base_url('backoffice/produtos')),
 		        );
-                break;
-                case 'medico':
-                case 'enfermeiros':
-                    $this->data['menuRoutes'] = array(
-                        array('name' => 'Utentes','path' => base_url('backoffice/utentes')),
-                        array('name' => 'Receitas','path' => base_url('backoffice/receitas')),
-                        array('name' => 'Produtos','path' => base_url('backoffice/produtos')),
-		            );
-                break;
-                case 'utente':
-                    $this->data['menuRoutes'] = array();
-                    break;
-                case 'rececionista':
-                    $this->data['menuRoutes'] = array(
-                        array('name' => 'Utentes','path' => base_url('backoffice/utentes')),
-		            );
-                break;
         }
-
+        if($tipo !== 'utente'){
+            array_push($this->data['menuRoutes'],array('name' => 'Utentes','path' => base_url('backoffice/utentes')),
+            array('name' => 'Receitas','path' => base_url('backoffice/receitas')),
+            );
+        }
         array_push($this->data['menuRoutes'],
                 array('name' => 'Perfil','path' => base_url('backoffice/users')),
+                array('name' => 'Medicos','path' => base_url('backoffice/medicos')),
+                array('name' => 'Enfermeiros','path' => base_url('backoffice/enfermeiros')),
                 array('name' => 'Consultas','path' => base_url('backoffice/consultas')),
                 array('name' => 'Logout','path' => base_url('logout')),
-        );
+            );
         $this->data['title'] = 'BackOffice';
         $this->data['css'] = base_url("resources/css/backoffice.css");
 
